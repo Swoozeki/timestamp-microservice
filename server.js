6b4d272233b3886d588233c5bf4d9baf2bfb0b59
Swoozeki <ka.sujit@gmail.com>
2016-07-10 20:50:03 +0000

    finished app

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 var express= require("express")
+var errorHandler= require("express-error-middleware")
 var app=express()
 
-app.use('/',function(req,res){
-    res.end("Hello World!")
-}).listen(8080,function(){
+app.use(express.static(__dirname))
+app.get('/:str',function(req,res){
+    var reqstr=req.params.str
+    var isUnix= reqstr.search(/[a-z]|[A-Z]/)==-1?true:false
+    
+    var jsonDates= {}
+
+    if(!isUnix && new Date(reqstr)=="Invalid Date")
+        jsonDates={
+            unix: null,
+            natural: null
+        }
+    else if(isUnix) 
+        jsonDates={
+            unix: reqstr,
+            natural: new Date(reqstr*1000).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric' })
+        }
+    else 
+        jsonDates={
+            unix: Date.parse(reqstr)/1000,
+            natural: new Date(reqstr).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric' })
+        }
+
+    
+    res.end(JSON.stringify(jsonDates))
+})
+
+app.get('*', errorHandler.NotFoundMiddleware)
+
+app.listen(8080,function(){
     console.log("listening on port 8080")
 })
+
